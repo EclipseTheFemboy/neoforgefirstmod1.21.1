8163947c1100cbbe02caffeb8ffd191b315f835d
Eclipse <danieledp007@gmail.com>
2025-05-20 01:47:47 -0700

    chisel item

---
+++ src/main/java/net/rizzosciacca/firstmod/item/custom/chisel/ChiselItem.java
@@ -0,0 +1,43 @@
+package net.rizzosciacca.firstmod.item.custom.chisel;
+
+import net.minecraft.world.InteractionResult;
+import net.minecraft.world.item.Item;
+import net.minecraft.world.item.context.UseOnContext;
+import net.minecraft.world.level.Level;
+import net.minecraft.world.level.block.Block;
+import net.minecraft.world.level.block.Blocks;
+
+import java.util.Map;
+
+public class ChiselItem extends Item {
+    private static final Map<Block, Block> CHISEL_MAP =
+            Map.of(
+                    Blocks.STONE, Blocks.STONE_BRICKS,
+                    Blocks.STONE_BRICKS, Blocks.STONE_BRICK_STAIRS,
+                    Blocks.STONE_BRICK_STAIRS, Blocks.STONE_BRICK_SLAB,
+                    Blocks.END_STONE, Blocks.END_STONE_BRICKS,
+                    Blocks.END_STONE_BRICKS, Blocks.END_STONE_BRICK_STAIRS,
+                    Blocks.END_STONE_BRICK_STAIRS, Blocks.END_STONE_BRICK_STAIRS,Blocks.END_STONE_BRICK_SLAB,
+                    Blocks.DEEPSLATE, Blocks.DEEPSLATE_BRICKS,
+                    Blocks.DEEPSLATE_BRICKS, Blocks.DEEPSLATE_BRICK_STAIRS,
+                    Blocks.DEEPSLATE_BRICK_STAIRS, Blocks.DEEPSLATE_BRICK_SLAB,
+
+            )
+    public ChiselItem(Properties properties) {
+        super(properties);
+    }
+
+    @Override
+    public InteractionResult useOn(UseOnContext context) {
+        Level level = context.getLevel();
+        Block clickedBlock = level.getBlockState(context.getClickedPos()).getBlock()
+
+            if (CHISEL_MAP.containsKey(clickedBlock)){
+                if (!level.isClientSide()) { // only on server side
+                    // if (level.isClientSide()) : only on client side
+                }
+            }
+
+        return super.useOn(context);
+    }
+}
